872583d3b5674a7c75cb0e2122e0e30c0157c68f
Leonardo Iacovini <leoiacovini@gmail.com>
2018-01-15 04:26:23 -0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject labsoft-service/lein-template "0.5.5"
+(defproject labsoft-service/lein-template "0.5.6"
   :description "Template for creating microservices using common-labsoft"
   :url "https://github.com/labsoft-2018/labsoft-service-template"
   :deploy-repositories [["clojars" {:url      "https://clojars.org/repo"
