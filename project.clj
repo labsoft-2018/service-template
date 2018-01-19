5d9c4c03c64b524032f251090e58d618ccad1a25
Leonardo Iacovini <leoiacovini@gmail.com>
2018-01-18 23:03:47 -0200

    Fix test-helpers

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject labsoft-service/lein-template "0.6.0"
+(defproject labsoft-service/lein-template "0.6.1"
   :description "Template for creating microservices using common-labsoft"
   :url "https://github.com/labsoft-2018/labsoft-service-template"
   :deploy-repositories [["clojars" {:url      "https://clojars.org/repo"
